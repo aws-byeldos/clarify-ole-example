812521c116442fea3c3ff1c0d80b2e77497c926f
Yeldos Balgabekov <byeldos@amazon.com>
2024-01-24 15:00:30 +0100

    inference toolkit

---
+++ model_handler.py
@@ -0,0 +1,27 @@
+import json
+import logging
+import random
+from typing import List
+
+
+def _get_prediction(features: List[str]) -> str:
+    item_description = features[0]
+    return f"auto-generated-description-for-{item_description}"
+
+
+def handle(data, context):
+    logging.info(f"data: {data}")
+    logging.info(f"context: {context}")
+    if data is None:  # first called during init, without the payload 'data'
+        return None
+    records = data[0]["body"]
+    data = json.loads(records.decode("utf-8"))
+    logging.info(f"data: {data}")
+
+    features = data["features"]
+    return [
+        {
+            "predictions": _get_prediction(features),
+            "probabilities": random.random()
+        }
+    ]
